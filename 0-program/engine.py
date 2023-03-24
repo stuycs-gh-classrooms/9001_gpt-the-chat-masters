c6a4e48ea4f18f4c0d5586fb37d13a920c62d708
vernon-hughes <78610763+vernon-hughes@users.noreply.github.com>
2023-03-24 15:33:03 -0400

    Update engine.py

--- 0-program/engine.py
+++ 0-program/engine.py
@@ -1,90 +1,71 @@
 import numpy as np
 from PIL import Image
-from math import floor
+import subprocess
 
-# Define canvas size
-canvas_width = 500
-canvas_height = 500
+def bresenham_line_algorithm(x1, y1, x2, y2, z):
+    """Bresenham's Line Algorithm"""
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    sx = 1 if x1 < x2 else -1
+    sy = 1 if y1 < y2 else -1
+    err = dx - dy
+    while x1 != x2 or y1 != y2:
+        image_array[x1, y1] = z  # draw the pixel on the image
+        e2 = 2 * err
+        if e2 > -dy:
+            err -= dy
+            x1 += sx
+        if e2 < dx:
+            err += dx
+            y1 += sy
+    image_array[x1, y1] = z  # draw the last pixel on the image
 
-# Create canvas with black background
-canvas = np.zeros((canvas_height, canvas_width, 3), dtype=np.uint8)
+def add_edge(edge_list, x1, y1, z1, x2, y2, z2):
+    """Add two points to the edge list"""
+    edge_list.append([x1, y1, z1, 1])
+    edge_list.append([x2, y2, z2, 1])
 
-# Define Bresenham's line algorithm
-def bresenham_line(x1, y1, x2, y2):
-    dx = x2 - x1
-    dy = y2 - y1
-    xsign = 1 if dx > 0 else -1
-    ysign = 1 if dy > 0 else -1
-    dx = abs(dx)
-    dy = abs(dy)
-    if dx > dy:
-        xx, xy, yx, yy = xsign, 0, 0, ysign
-    else:
-        dx, dy = dy, dx
-        xx, xy, yx, yy = 0, ysign, xsign, 0
-    D = 2*dy - dx
-    y = 0
-    for x in range(dx + 1):
-        yield x1 + x*xx + y*yx, y1 + x*xy + y*yy
-        if D >= 0:
-            y += 1
-            D -= 2*dx
-        D += 2*dy
+def create_translation_matrix(tx, ty, tz):
+    """Create a 4x4 translation matrix"""
+    return np.array([[1, 0, 0, tx],
+                     [0, 1, 0, ty],
+                     [0, 0, 1, tz],
+                     [0, 0, 0, 1]])
 
-# Define edge list
-edge_list = []
+def create_rotation_matrix_x(theta):
+    """Create a 4x4 rotation matrix around the x-axis"""
+    cos_t = np.cos(theta)
+    sin_t = np.sin(theta)
+    return np.array([[1, 0, 0, 0],
+                     [0, cos_t, -sin_t, 0],
+                     [0, sin_t, cos_t, 0],
+                     [0, 0, 0, 1]])
 
-# Define functions for creating matrices
-def translation_matrix(tx, ty, tz):
-    return np.array([
-        [1, 0, 0, tx],
-        [0, 1, 0, ty],
-        [0, 0, 1, tz],
-        [0, 0, 0, 1]
-    ])
+def create_rotation_matrix_y(theta):
+    """Create a 4x4 rotation matrix around the y-axis"""
+    cos_t = np.cos(theta)
+    sin_t = np.sin(theta)
+    return np.array([[cos_t, 0, sin_t, 0],
+                     [0, 1, 0, 0],
+                     [-sin_t, 0, cos_t, 0],
+                     [0, 0, 0, 1]])
 
-def rotation_matrix_x(theta):
-    return np.array([
-        [1, 0, 0, 0],
-        [0, np.cos(theta), -np.sin(theta), 0],
-        [0, np.sin(theta), np.cos(theta), 0],
-        [0, 0, 0, 1]
-    ])
+def create_rotation_matrix_z(theta):
+    """Create a 4x4 rotation matrix around the z-axis"""
+    cos_t = np.cos(theta)
+    sin_t = np.sin(theta)
+    return np.array([[cos_t, -sin_t, 0, 0],
+                     [sin_t, cos_t, 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 1]])
 
-def rotation_matrix_y(theta):
-    return np.array([
-        [np.cos(theta), 0, np.sin(theta), 0],
-        [0, 1, 0, 0],
-        [-np.sin(theta), 0, np.cos(theta), 0],
-        [0, 0, 0, 1]
-    ])
+def create_dilation_matrix(sx, sy, sz):
+    """Create a 4x4 dilation matrix"""
+    return np.array([[sx, 0, 0, 0],
+                     [0, sy, 0, 0],
+                     [0, 0, sz, 0],
+                     [0, 0, 0, 1]])
 
-def rotation_matrix_z(theta):
-    return np.array([
-        [np.cos(theta), -np.sin(theta), 0, 0],
-        [np.sin(theta), np.cos(theta), 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]
-    ])
-
-def dilation_matrix(sx, sy, sz):
-    return np.array([
-        [sx, 0, 0, 0],
-        [0, sy, 0, 0],
-        [0, 0, sz, 0],
-        [0, 0, 0, 1]
-    ])
-
-# Define function for saving PPM files as PNGs
-def save_ppm_as_png(filename, ppm_data):
-    img = Image.fromarray(ppm_data, mode='RGB')
-    img.save(filename)
-
-# Define function for displaying images using ImageMagick
-def display_image(filename):
-    from subprocess import run
-    run(['display', filename])
-
-# Define function for drawing circles
-def draw_circle(center_x, center_y, radius, color):
-    for x,
+def save_image(filename):
+    """Save the image as a PPM file and convert it to a PNG file"""
+    image = Image.fromarray(np.uint8(image_array * 255

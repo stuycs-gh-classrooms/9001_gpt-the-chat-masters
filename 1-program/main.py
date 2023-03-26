1c75eaffe0041ca0490c3baee903b8d30d6b8a7c
vernon-hughes <vhughes30@stuy.edu>
2023-03-26 11:08:37 -0400

    i hate chat gpt

--- 1-program/main.py
+++ 1-program/main.py
@@ -2,7 +2,4 @@ from parser import parse_script
 
 filename = "script"
 
-with open(filename, "r") as f:
-    script = f.readlines()
-
-parse_script(script)
+parse_script(filename)
--- 1-program/parser.py
+++ 1-program/parser.py
@@ -1,49 +1,47 @@
 from typing import List, Tuple
 from transformation_matrices import *
 
-def parse_script(script: str) -> None:
-    edge_matrix = []
-    transform_matrix = identity_matrix()
+def parse_script(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
 
-    lines = script.strip().split('\n')
+    transform = identity_matrix()
+    edges = []
     for i in range(len(lines)):
-        parts = lines[i].strip().split()
-        if len(parts) > 0:
-            cmd = parts[0]
-            args = []
-            if cmd == 'line' and len(parts) == 7:
-                args = list(map(float, parts[1:]))
-                add_edge(edge_matrix, *args)
-            elif cmd == 'scale' and len(parts) == 4:
-                args = list(map(float, parts[1:]))
-                scale_matrix = create_dilation_matrix(*args)
-                transform_matrix = multiply_matrices(transform_matrix, scale_matrix)
-            elif cmd == 'move' and len(parts) == 4:
-                args = list(map(float, parts[1:]))
-                translation_matrix = create_translation_matrix(*args)
-                transform_matrix = multiply_matrices(transform_matrix, translation_matrix)
-            elif cmd == 'rotate' and len(parts) == 3:
-                axis = parts[1]
-                theta = float(parts[2])
-                if axis == 'x':
-                    rotation_matrix = create_rotation_matrix_x(theta)
-                elif axis == 'y':
-                    rotation_matrix = create_rotation_matrix_y(theta)
-                elif axis == 'z':
-                    rotation_matrix = create_rotation_matrix_z(theta)
-                transform_matrix = multiply_matrices(transform_matrix, rotation_matrix)
-            elif cmd == 'apply' and len(parts) == 1:
-                apply_matrix(edge_matrix, transform_matrix)
-            elif cmd == 'display' and len(parts) == 1:
-                clear_screen()
-                draw_lines(edge_matrix)
-                display_screen()
-            elif cmd == 'save' and len(parts) == 2:
-                file_name = parts[1]
-                clear_screen()
-                draw_lines(edge_matrix)
-                save_extension(file_name)
-            elif cmd == 'ident' and len(parts) == 1:
-                transform_matrix = identity_matrix()
-            else:
-                raise ValueError(f"Invalid command: {lines[i]}")
+        command = lines[i].strip()
+        if command == 'line':
+            args = list(map(int, lines[i+1].split()))
+            add_edge(edges, args[0], args[1], args[2], args[3], args[4], args[5])
+            i += 1
+        elif command == 'ident':
+            transform = identity_matrix()
+        elif command == 'scale':
+            args = list(map(float, lines[i+1].split()))
+            transform = matrix_mult(transform, create_dilation_matrix(args[0], args[1], args[2]))
+            i += 1
+        elif command == 'move':
+            args = list(map(float, lines[i+1].split()))
+            transform = matrix_mult(transform, create_translation_matrix(args[0], args[1], args[2]))
+            i += 1
+        elif command == 'rotate':
+            args = lines[i+1].split()
+            if args[0] == 'x':
+                transform = matrix_mult(transform, create_rotation_matrix_x(float(args[1])))
+            elif args[0] == 'y':
+                transform = matrix_mult(transform, create_rotation_matrix_y(float(args[1])))
+            elif args[0] == 'z':
+                transform = matrix_mult(transform, create_rotation_matrix_z(float(args[1])))
+            i += 1
+        elif command == 'apply':
+            edges = matrix_mult(transform, edges)
+        elif command == 'display':
+            clear_screen(screen)
+            draw_lines(edges, screen, color)
+            display(screen)
+        elif command == 'save':
+            args = lines[i+1].strip()
+            clear_screen(screen)
+            draw_lines(edges, screen, color)
+            save_ppm(screen, args)
+            i += 1
+    return edges
